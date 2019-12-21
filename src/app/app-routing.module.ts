8cde974744d5ac62d65b620ec8c767d4b360c442
randolphpebenito <randolphpebenito@gmail.com>
2019-12-21 22:50:58 +0800

    feat: 🎸 Angular setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //App default redirect page
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+
+  //App set/default Layout: Dashboard layout
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { SharedModule } from './shared/shared.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
